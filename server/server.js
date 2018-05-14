00f21dba69c1e5d80b41675e5ceb627a3e8d40c0
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2018-05-14 16:36:46 +0100

    server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 /* eslint-disable */
 const le_store_certbot = require('le-store-certbot')
-const Greenlock = require('greenlock-express');
+const Greenlock = require('greenlock');
 const redirect_https = require('redirect-https')
 const express = require('express')
 const util = require('util')
+const os = require('os')
+const path = require('path')
 const servers = {
   http: require('http'),
   https: require('https'),
   spdy: require('http2'),
   prox: require('http-proxy')
 }
-
-var chal1 =  le_challenge_fs.create({
-  webrootPath: '~/letsencrypt/var/acme-challenges'})
 
 module.exports = function server(config) {
   var o = {
@@ -23,7 +22,7 @@ module.exports = function server(config) {
   function init() {
     function approveDomains(opts, certs, cb) {
       // TODO - verify domain
-      console.log('opts.....')
+      console.log('opts.....', opts.domain)
       if (!o.config.router[opts.domain]) {
         cb(true)
         return
@@ -37,25 +36,10 @@ module.exports = function server(config) {
       cb(null, { options: opts, certs: certs })
     }
 
-    var leStore = le_store_certbot.create({
-      configDir: '~/letsencrypt/etc',
-      privkeyPath: ':configDir/live/:hostname/privkey.pem',
-      fullchainPath: ':configDir/live/:hostname/fullchain.pem',
-      certPath: ':configDir/live/:hostname/cert.pem',
-      chainPath: ':configDir/live/:hostname/chain.pem',
-      workDir: '~/letsencrypt/var/lib',
-      logsDir: '~/letsencrypt/var/log',
-      webrootPath: '~/letsencrypt/srv/www/:hostname/.well-known/acme-challenge',
-      debug: o.config.debug
-    })
-
     const greenlock = Greenlock.create({
       server: o.config.production ? 'https://acme-v02.api.letsencrypt.org/directory': 'https://acme-staging-v02.api.letsencrypt.org/directory',
       version: 'draft-11',
-      challenges: {
-        'draft-11': chal1,
-      },
-      store: leStore,
+      configDir: path.join('/root/acme/etc'),
       approveDomains
     })
 
@@ -67,8 +51,10 @@ module.exports = function server(config) {
         req.destroy()
         return
       }
-      if (!isNaN(a) || (!a.http && !isNaN(a.port) && !a.redirect) ) {
-        proxy.web(req, res, {target: 'http://localhost:'+a})
+      const bNumber = typeof a === 'number'
+      const port =  bNumber ? a : a.port
+      if (bNumber || (!a.http && !isNaN(a.port) && !a.redirect) ) {
+        proxy.web(req, res, {target: 'http://localhost:'+port})
         return
       }
       if (a.redirect === true) {
@@ -84,12 +70,12 @@ module.exports = function server(config) {
 
     var proxy = servers.prox.createProxyServer({ws: true})
     proxy.on('error', function(e){
-      console.log(e.Error)
+      console.log('proxy error', e.Error)
     })
 
     // handles acme-challenge and redirects to https
-    const redir = greenlock.middleware(redirect_https({ port: o.config.ports.https }))
-    o.http = servers['http'].createServer(function (req, res) {
+    const redir = redirect_https({ port: o.config.ports.https })
+    o.http = servers['http'].createServer(greenlock.middleware(function (req, res) {
       const a = o.config.router[req.headers.host]
       if (o.config.debug) {console.log('http', req.headers.host)}
       if  (!a) {
@@ -110,7 +96,7 @@ module.exports = function server(config) {
         return
       }
       req.destroy()
-    })
+    }))
     o.http.on('upgrade', function(req, socket, head) {
       if (o.config.debug) {console.log('http upgrade', req.headers.host)}
       var a = o.config.router[req.headers.host]
@@ -122,7 +108,7 @@ module.exports = function server(config) {
       return
     })
     
-    o.https = servers[o.config.spdy ? 'spdy' : 'https'].createServer(greenlock.httpsOptions, greenlock.middleware(app))
+    o.https = servers[o.config.spdy ? 'spdy' : 'https'].createServer(greenlock.httpsOptions, app)
     o.https.on('upgrade', function(req, socket, head) {
       var a = o.config.router[req.headers.host]
       if (o.config.debug) {console.log('https upgrade', req.headers.host)}
